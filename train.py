e3d0917ba497dd18dcf1fe9b24cd59f6f23d8764
KarlKangYu <karl.kang@kikatech.com>
2018-07-25 18:38:11 +0800

    vdcnn pos_tags deps head_word

--- train.py
+++ train.py
@@ -39,11 +39,11 @@ tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device 
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
 
 FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("Parameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-	print("{}={}".format(attr, value))
-print("")
+# FLAGS._parse_flags()
+# print("Parameters:")
+# for attr, value in sorted(FLAGS.__flags.items()):
+# 	print("{}={}".format(attr, value))
+# print("")
 
 def train():
 	# Data Preparation
